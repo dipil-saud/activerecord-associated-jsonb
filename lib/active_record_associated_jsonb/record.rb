a27d78d1b61fe7a78cfc82ffb0d0c2f65be217bf
Dipil Singh Saud <dipil.saud@gmail.com>
2020-05-24 19:41:37 +0545

    Updated rubocop config file and fixed violations.

--- lib/active_record_associated_jsonb/record.rb
+++ lib/active_record_associated_jsonb/record.rb
@@ -11,73 +11,71 @@
 #
 #   validates :name, presence: true
 # end
-class ActiveRecordAssociatedJsonb::Record
-  include ActiveModel::Model
-  include ActiveModel::Serializers::JSON
-  include ActiveModel::Dirty
+module ActiveRecordAssociatedJsonb
+  class Record
+    include ActiveModel::Model
+    include ActiveModel::Serializers::JSON
+    include ActiveModel::Dirty
 
-  # @return [Array<String>] Stores the list of attributes for the class
-  def self.attributes_list
-    @attributes_list ||= []
-  end
-
-  # Define an attribute for the class
-  # Creates getters and setters
-  #
-  # @param attribute_name [Symbol]
-  # @param options [Hash]
-  #   * :default [String] Default value for the attribute
-  #   * :type [Symbol] Type of attribute to cast to. Should be one of ActiveModel::Type
-  def self.attribute(attribute_name, options = {})
-    attributes_list << attribute_name
-    define_attribute_method attribute_name # required by ActiveModel::Dirty
-
-    define_attribute_getter(attribute_name, options)
-    define_attribute_setter(attribute_name, options)
-    define_attribute_checker(attribute_name)
-  end
-
-  # @returns [Hash] List of attributes and values for serialization
-  def attributes
-    self.class.attributes_list.each_with_object({}) do |property, attributes_hash|
-      attributes_hash[property] = send(property)
-      attributes_hash
+    # @return [Array<String>] Stores the list of attributes for the class
+    def self.attributes_list
+      @attributes_list ||= []
     end
-  end
 
-  class << self
-    # Defines the getter method for the attribute
-    # Converts the value to specified type when returning
-    def define_attribute_getter(attribute_name, options)
-      define_method attribute_name do
-        value = instance_variable_get("@#{attribute_name}")
-        value = options[:default] if value.nil?
+    # Define an attribute for the class
+    # Creates getters and setters
+    #
+    # @param attribute_name [Symbol]
+    # @param options [Hash]
+    #   * :default [String] Default value for the attribute
+    #   * :type [Symbol] Type of attribute to cast to. Should be one of ActiveModel::Type
+    def self.attribute(attribute_name, options = {})
+      attributes_list << attribute_name
+      define_attribute_method attribute_name # required by ActiveModel::Dirty
 
-        if options[:type]
-          value = ActiveModel::Type.lookup(options[:type]).cast(value)
-        end
+      define_attribute_getter(attribute_name, options)
+      define_attribute_setter(attribute_name, options)
+      define_attribute_checker(attribute_name)
+    end
 
-        value
+    # @returns [Hash] List of attributes and values for serialization
+    def attributes
+      self.class.attributes_list.each_with_object({}) do |property, attributes_hash|
+        attributes_hash[property] = send(property)
+        attributes_hash
       end
     end
 
-    # Defines the setter method for the attribute
-    def define_attribute_setter(attribute_name, options)
-      define_method "#{attribute_name}=" do |value|
-        send("#{attribute_name}_will_change!") unless value == send(attribute_name)
+    class << self
+      # Defines the getter method for the attribute
+      # Converts the value to specified type when returning
+      def define_attribute_getter(attribute_name, options)
+        define_method attribute_name do
+          value = instance_variable_get("@#{attribute_name}")
+          value = options[:default] if value.nil?
 
-        if options[:type]
-          value = ActiveModel::Type.lookup(options[:type]).deserialize(value)
+          value = ActiveModel::Type.lookup(options[:type]).cast(value) if options[:type]
+
+          value
         end
-
-        instance_variable_set("@#{attribute_name}", value)
       end
-    end
 
-    # Defines the checker method which returns true if the attribute is set
-    def define_attribute_checker(attribute_name)
-      define_method "#{attribute_name}?" do
-        send(attribute_name).present?
+      # Defines the setter method for the attribute
+      def define_attribute_setter(attribute_name, options)
+        define_method "#{attribute_name}=" do |value|
+          send("#{attribute_name}_will_change!") unless value == send(attribute_name)
+
+          value = ActiveModel::Type.lookup(options[:type]).deserialize(value) if options[:type]
+
+          instance_variable_set("@#{attribute_name}", value)
+        end
+      end
+
+      # Defines the checker method which returns true if the attribute is set
+      def define_attribute_checker(attribute_name)
+        define_method "#{attribute_name}?" do
+          send(attribute_name).present?
+        end
       end
     end
   end
